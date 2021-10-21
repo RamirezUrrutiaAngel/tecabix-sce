32921b94724b1b99e6a03e956cf7b461e07a135c
Angel Abinadí Ramirez Urrutia <abinadi.ramirez.u@gmail.com>
2021-10-20 21:26:21 -0500

    Cuenta

---
+++ tecabix-sce/src/main/java/mx/tecabix/db/entity/Cuenta.java
@@ -0,0 +1,147 @@
+/*
+ *   This file is part of Foobar.
+ *
+ *   Foobar is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Foobar is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+package mx.tecabix.db.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+/**
+ * 
+ * @author Ramirez Urrutia Angel Abinadi
+ * 
+ */
+@Entity()
+@Table(name = "cuenta")
+public class Cuenta implements Serializable{
+
+	private static final long serialVersionUID = -4711461989562797237L;
+	
+	@Id
+	@JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "id_cuenta", unique = true, nullable = false)
+	@SequenceGenerator(name = "cuenta_id_cuenta_gen", sequenceName = "tecabix_sce.cuenta_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cuenta_id_cuenta_gen")
+	private Integer id;
+	@ManyToOne
+    @JoinColumn(name = "id_persona")
+	@JsonProperty(access = Access.WRITE_ONLY)
+    private Persona persona;
+    @Column(name = "saldo")
+    private Integer saldo;
+    @Column(name = "fecha_expiracion")
+    private LocalDateTime fechaDeExpiracion;
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "id_usuario_modificado")
+    private Long idUsuarioModificado;
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "fecha_modificado")
+    private LocalDateTime fechaDeModificacion;
+    @ManyToOne
+    @JoinColumn(name = "id_estatus")
+    private Catalogo estatus;
+    @Column(name = "clave")
+    @Type(type="pg-uuid")
+    private UUID clave;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Persona getPersona() {
+		return persona;
+	}
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+	public Integer getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Integer saldo) {
+		this.saldo = saldo;
+	}
+	public LocalDateTime getFechaDeExpiracion() {
+		return fechaDeExpiracion;
+	}
+	public void setFechaDeExpiracion(LocalDateTime fechaDeExpiracion) {
+		this.fechaDeExpiracion = fechaDeExpiracion;
+	}
+	public Long getIdUsuarioModificado() {
+		return idUsuarioModificado;
+	}
+	public void setIdUsuarioModificado(Long idUsuarioModificado) {
+		this.idUsuarioModificado = idUsuarioModificado;
+	}
+	public LocalDateTime getFechaDeModificacion() {
+		return fechaDeModificacion;
+	}
+	public void setFechaDeModificacion(LocalDateTime fechaDeModificacion) {
+		this.fechaDeModificacion = fechaDeModificacion;
+	}
+	public Catalogo getEstatus() {
+		return estatus;
+	}
+	public void setEstatus(Catalogo estatus) {
+		this.estatus = estatus;
+	}
+	public UUID getClave() {
+		return clave;
+	}
+	public void setClave(UUID clave) {
+		this.clave = clave;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((clave == null) ? 0 : clave.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cuenta other = (Cuenta) obj;
+		if (clave == null) {
+			if (other.clave != null)
+				return false;
+		} else if (!clave.equals(other.clave))
+			return false;
+		return true;
+	}
+}
